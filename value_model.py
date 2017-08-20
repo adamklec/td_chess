44ca1d68192e8c043477c9d0205c88287dd439a9
Adam Kleczewski <adam.kleczewski@human.x.ai>
2017-08-20 13:55:57 -0400

    xavier initi

--- value_model.py
+++ value_model.py
@@ -6,13 +6,12 @@ class ValueModel(object):
 
         with tf.variable_scope('neural_network'):
             self.feature_vector_ = tf.placeholder(tf.float32, shape=[None, input_dim], name='feature_vector_')
-
             with tf.variable_scope('layer_1'):
-                W_1 = tf.get_variable('W_1', initializer=tf.truncated_normal([input_dim, hidden_dim], stddev=0.01))
+                W_1 = tf.get_variable('W_1', shape=[input_dim, hidden_dim], initializer=tf.contrib.layers.xavier_initializer())
                 self.simple_learned = tf.matmul(self.feature_vector_, W_1)
                 hidden = tf.nn.relu(tf.matmul(self.feature_vector_, W_1), name='hidden')
             with tf.variable_scope('layer_2'):
-                W_2 = tf.get_variable('W_2', initializer=tf.truncated_normal([hidden_dim, 1], stddev=0.01))
+                W_2 = tf.get_variable('W_2', shape=[hidden_dim, 1], initializer=tf.contrib.layers.xavier_initializer())
                 self.hidden = tf.matmul(hidden, W_2)
 
             self.value = tf.tanh(self.hidden)
