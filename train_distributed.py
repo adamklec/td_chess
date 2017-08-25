50d725e5378f768f11035601595659ed0cf56eeb
Adam Kleczewski <adam.kleczewski@human.x.ai>
2017-08-25 16:12:31 -0400

    no sleep

--- train_distributed.py
+++ train_distributed.py
@@ -78,7 +78,6 @@ if __name__ == "__main__":
         p = Process(target=work, args=(env, 'worker', task_idx, cluster_spec, ckpt_dir,))
         processes.append(p)
         p.start()
-        time.sleep(.5)
 
     for process in processes:
         process.join()
