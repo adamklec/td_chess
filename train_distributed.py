ee73565637d3965eb3d5b521e45ac8de66b0a5d2
Adam Kleczewski <adam.kleczewski@human.x.ai>
2017-09-06 23:18:53 -0400

    alright

--- train_distributed.py
+++ train_distributed.py
@@ -64,8 +64,8 @@ if __name__ == "__main__":
     this_ip = args.ips[0]
     that_ip = args.ips[1]
 
-    ps_hosts = [this_ip + ':' + str(2222 + i) for i in range(5)]# + [that_ip + ':' + str(2222 + i) for i in range(5)]
-    worker_hosts = [this_ip + ':' + str(3333 + i) for i in range(40)]# + [that_ip + ':' + str(3333 + i) for i in range(40)]
+    ps_hosts = [this_ip + ':' + str(2222 + i) for i in range(5)] + [that_ip + ':' + str(2222 + i) for i in range(5)]
+    worker_hosts = [this_ip + ':' + str(3333 + i) for i in range(40)] + [that_ip + ':' + str(3333 + i) for i in range(40)]
 
     ckpt_dir = "./log/" + args.run_name
     cluster_spec = tf.train.ClusterSpec({"ps": ps_hosts, "worker": worker_hosts})
