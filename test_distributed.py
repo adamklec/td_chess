c8fd6ce6a9da9d73fcf3a1c467f7b89601fd2c2a
Adam Kleczewski <adam.kleczewski@human.x.ai>
2017-09-10 12:45:48 -0400

    35 testers

--- test_distributed.py
+++ test_distributed.py
@@ -112,7 +112,7 @@ if __name__ == "__main__":
     ps_hosts = [args.chief_ip + ':' + str(2222 + i) for i in range(5)]
     chief_trainer_hosts = [args.chief_ip + ':' + str(3333 + i) for i in range(40)]
     worker_trainer_hosts = [args.worker_ip + ':' + str(3333 + i) for i in range(40)]
-    tester_hosts = [args.tester_ip + ':' + str(3333 + i) for i in range(40)]
+    tester_hosts = [args.tester_ip + ':' + str(3333 + i) for i in range(35)]
 
     ckpt_dir = "./log/" + args.run_name
     cluster_spec = tf.train.ClusterSpec(
